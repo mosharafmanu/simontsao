45a59f5677a0a14d14f00d504a4f7070ba6dcd45
Mosharaf Hossain <mosharafhussain.manu@gmail.com>
2026-03-31 15:59:53 +0600

    quote section list added.

--- template-parts/sections/quote_content_section.php
+++ template-parts/sections/quote_content_section.php
@@ -9,11 +9,56 @@ $section_heading = get_sub_field( 'section_heading' );
 $quote_content   = get_sub_field( 'quote_content' );
 $body_content    = get_sub_field( 'body_content' );
 $bottom_note     = get_sub_field( 'bottom_note' );
+$list_items      = get_sub_field( 'list_items' );
 $quote_position  = get_sub_field( 'quote_position' ) ?: 'left';
 $bottom_style    = get_sub_field( 'bottom_content_style' ) ?: 'highlighted_note';
+$list_style      = get_sub_field( 'list_style_variant' ) ?: 'default';
 
 $quote_column_classes = 'col-sm-12 order-sm-1 col-lg-4 col-md-4 mt-lg-5 align-self-center';
 $body_column_classes  = 'col-sm-12 order-sm-2 col-lg-8 col-md-8';
+
+if ( ! function_exists( 'simontsao_render_quote_content_section_list_item' ) ) {
+	/**
+	 * Render a structured bottom list item for the quote content section.
+	 *
+	 * @param array  $item       Repeater row data.
+	 * @param string $list_style Current list style variant.
+	 * @return void
+	 */
+	function simontsao_render_quote_content_section_list_item( $item, $list_style ) {
+		$default_text        = $item['default_text'] ?? '';
+		$lead_text           = $item['lead_text'] ?? '';
+		$supporting_text     = $item['supporting_text'] ?? '';
+		$has_highlighted_row = 'highlighted_lead_text' === $list_style && ( $lead_text || $supporting_text );
+
+		if ( $has_highlighted_row ) {
+			?>
+			<li>
+				<p>
+					<?php if ( $lead_text ) : ?>
+						<span><?php echo esc_html( $lead_text ); ?></span>
+					<?php endif; ?>
+					<?php if ( $lead_text && $supporting_text ) : ?>
+						<span><?php echo esc_html( ' ' ); ?></span>
+					<?php endif; ?>
+					<?php if ( $supporting_text ) : ?>
+						<?php echo nl2br( esc_html( $supporting_text ) ); ?>
+					<?php endif; ?>
+				</p>
+			</li>
+			<?php
+			return;
+		}
+
+		if ( 'default' === $list_style && $default_text ) {
+			?>
+			<li>
+				<p><?php echo nl2br( esc_html( $default_text ) ); ?></p>
+			</li>
+			<?php
+		}
+	}
+}
 
 if ( 'right' === $quote_position ) {
 	$quote_column_classes .= ' order-lg-2';
@@ -52,6 +97,16 @@ if ( 'right' === $quote_position ) {
 						<div class="quote-content-section__bottom-content pt-5">
 							<?php echo wp_kses_post( $bottom_note ); ?>
 						</div>
+
+						<?php if ( $list_items ) : ?>
+							<div class="feature-content-section__list quote-content-section__bottom-list">
+								<ul class="custom-list<?php echo 'highlighted_lead_text' === $list_style ? ' feature-content-section__list--research' : ''; ?>">
+									<?php foreach ( $list_items as $item ) : ?>
+										<?php simontsao_render_quote_content_section_list_item( $item, $list_style ); ?>
+									<?php endforeach; ?>
+								</ul>
+							</div>
+						<?php endif; ?>
 					<?php else : ?>
 						<h5 class="pt-5 text-lg-center">
 							<?php echo wp_kses_post( $bottom_note ); ?>
